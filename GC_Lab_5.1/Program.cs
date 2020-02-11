1abb8936aee671d4e4a776f376c130b2632c42fa
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-11 09:33:54 -0500

    Created project

---
+++ GC_Lab_5.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GC_Lab_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
